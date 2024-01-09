a9b2755514a37b9925ddcf02dfb6756c78e7140d
kent riggs <k2r3@proton.me>
2024-01-09 09:31:04 -0700

    solved for tests

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride < 400) {
+    return "This one is on me!";
+} 
+else if (ride >= 400 && ride <=2000) { 
+  return "That will be twenty bucks."
+}
+else if (ride > 2500) {
+  return "No can do.";
+}
+else if (ride > 2000) {
+  return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let isNYC = city === "NYC";
+let response = isNYC ? "Ok, sounds good." : "No go.";
+return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+let response;
+
+switch (tipAmount) {
+  case "generous":
+    response = "Thank you so much.";
+    break;
+  case "not as generous":
+    response = "Thank you.";
+    break;
+  default:
+    response = "Bye.";
+}
+return response;
 }
